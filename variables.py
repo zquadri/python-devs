0f6aa5d5853ebfa062fab543b420e83b6dd554a6
Zaid Quadri <Zaid.Quadri@pearson.com>
2020-10-29 21:55:55 -0400

    first commit

---
+++ variables.py
@@ -0,0 +1,25 @@
+# THIS IS HOW I PRINT SOMETHING IN PYTHON
+
+
+#print('hello world')
+
+"""
+This function registers students to database
+"""
+
+
+
+
+
+
+
+def convert_farenheit_co_celsius():
+    x = 5 
+    y = 10
+    print(x)
+    print(y)
+
+print(5)
+
+
+# convert_farenheit_co_celsius()
